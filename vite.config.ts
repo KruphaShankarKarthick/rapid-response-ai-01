8993c55c763c68dfdb8e55a326f6cb91c100a151
Krupha Shankar Karthick <116346900+KruphaShankarKarthick@users.noreply.github.com>
2025-09-10 14:19:43 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import path from "path";
 
 export default defineConfig(async () => {
   const plugins = [react()];
@@ -12,5 +13,10 @@ export default defineConfig(async () => {
 
   return {
     plugins,
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "src"), // 👈 add this
+      },
+    },
   };
 });
